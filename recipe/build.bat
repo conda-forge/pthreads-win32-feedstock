33c92624a39c296b358dd8c838da9238869eb0d8
Koyo MUNECHIKA <munechika.koyo@gmail.com>
2026-02-10 12:40:33 +0100

    Refactor build.bat to improve build process and output directory creation

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,21 @@
 set "XCFLAGS=/W3 /MT /nologo"
 
 cd pthreads.2
-nmake /E clean VSE-inlined
 
+:: Build the VSE-inlined configuration
+nmake /E clean VSE-inlined
+if %ERRORLEVEL% neq 0 exit 1
+
+:: Build the static configuration
+nmake /E clean VC-static
+if %ERRORLEVEL% neq 0 exit 1
+
+:: Create the output directories
+mkdir %LIBRARY_BIN%
+mkdir %LIBRARY_INC%
+mkdir %LIBRARY_LIB%
+
+:: Copy the built files
 copy pthreadVSE2.lib %LIBRARY_LIB%\pthreads.lib
 copy pthreadVSE2.lib %LIBRARY_LIB%\pthread.lib
 copy pthreadVSE2.dll %LIBRARY_BIN%\pthreadVSE2.dll
@@ -11,7 +24,6 @@ copy pthread.h %LIBRARY_INC%\pthread.h
 copy sched.h %LIBRARY_INC%\sched.h
 copy semaphore.h %LIBRARY_INC%\semaphore.h
 
-nmake /E clean VC-static
 copy pthreadVC2.lib %LIBRARY_LIB%\pthreads_static.lib
 
 cd tests
